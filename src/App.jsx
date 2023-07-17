06f2e32de3e5f65573f157ec08f79f878728e233
Maximiliano <Maximiliano.Corbo394@gmail.com>
2023-07-17 11:17:26 -0300

    Index sin filtros

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
-
+import Pokeball from './assets/img/svg/pokeballwhite.svg'
+import Search from './assets/img/svg/search.svg'
+import Sort from './assets/img/svg/sort.svg'
 
 
 
@@ -13,17 +15,41 @@ function App() {
       method: 'GET',
       redirect: 'follow'
     };
-    
+    //Para todos lo pokemons usar en el fetch el link: https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0
     fetch("https://pokeapi.co/api/v2/pokemon/", requestOptions)
       .then(response => response.json())
       .then(result => setPokemons(result.results))
       .catch(error => console.log('error', error));  
   }, [])
-  console.log(pokemons)
+  
   return (
-    <>
-      {pokemons.length && pokemons.map((pokemon)=><PokemonCard name={pokemon.name} url={pokemon.url}/>)}
-    </>
+    <div className='Estructura .Primary'>
+
+      <header className='buscador'>
+        <div className='todo'>
+          <div>
+            <img src={Pokeball} alt="Logo"/>
+            <h1>Pokédex</h1>
+          </div>
+          
+          <button><img src={Sort} alt="" srcset="" /></button>
+        </div>
+        <div className='search'>
+          <img src={Search}/>
+          <input type="text" placeholder={`Buscar`}/>
+        </div>
+      </header>
+      
+      <div className='BodyWrapper'>
+        {
+        pokemons.length 
+        && 
+        pokemons.map((pokemon)=>
+        <PokemonCard name={pokemon.name} url={pokemon.url}/>)
+        }
+      </div>
+
+    </div>
   )
 }
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -16,19 +16,25 @@ const PokemonCard = ({name, url})=> {
       .then(result => setData(result))
       .catch(error => console.log('error', error));  
   }, [])
-  console.log(data)
 
     return(
-        data ? (<section className={data.types[0].type.name}>
-            <div className="top-section">
-                <h1>{data.name}</h1>
-                <h2>{data.id}</h2>
-                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`} alt="" />
-            </div>
-            <div className="bottom-section">
-                <h2>{data.name}</h2>
-            </div>
-        </section>) : <h1> Loading {name} </h1>
+        data ? (
+                <section className={`${data.types[0].type.name} seccion`}>
+                    <div className="top-section">
+                        <h2>#{data.id}</h2>
+                        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`} alt="" />
+                        {/* <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`} alt="" />
+                        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${data.id}.gif`} alt="" /> */}  
+                    </div>
+
+                    <div className="bottom-section">
+                        <h2>{data.name}</h2>
+                    </div>
+                </section>
+                
+                ) : (
+                <h1> Loading {name} </h1>
+                )
     )
 }
 
