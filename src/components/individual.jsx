b104282a7eb77ee00049d9e33b80b087f1ab3966
Maximiliano <Maximiliano.Corbo394@gmail.com>
2023-07-19 16:15:41 -0300

    Se diseñan las tarjetas individuales

--- src/components/individual.jsx
+++ src/components/individual.jsx
@@ -21,29 +21,40 @@ const Individual = () => {
         method: "GET",
         redirect: "follow",
       };
-      await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, requestOptions)
+      const fetching = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, requestOptions)
         .then((response) => response.json())
-        .then((result) => setData(result))
-        .catch((error) => console.log("error", error));
+        // .then (result =>
+        //   setData(result))
+        //(async(result) => {
+      //   const species= await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
+      //   const respuesta= species.json()
+      //   console.log(respuesta);
+      //   return {
+      //     ...result, descripcion: respuesta
+      //   }
+      // })
+      .catch((error) => console.log("error", error));
+      const species= await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((response) => response.json())
+      setData ({...fetching, descripcion:species.flavor_text_entries[26].flavor_text})
     };
     getPokemon ()
   }, []);
   console.log(data);
   return (
-    <div className={`${data.types[0].type.name} detalles`}>
+    <div className={`${data?.types[0].type.name} detalles`}>
       <div className="titulo">
         <button className="flecha-atras">
           <img src={flechaAtras} alt="" />
         </button>
         <span className="nombre">{data ? data.name : <Skeleton />}</span>
-        <span className="numero">{data ? `# ${data.id}` : <Skeleton />}</span>
+        <span className="numero">{data ? `# ${data.id}` : <Skeleton count={3} />}</span>
       </div>
       <header>
         <button className="anterior">
           <img src={anterior} alt="" />
         </button>
-        <div className="sombraImagen">
-          {<img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`} alt="" />}
+        <div className="Imagen">
+          {<img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data?.id}.png`} alt="" />}
         </div>
         <button className="posterior">
           <img src={posterior} alt="" />
@@ -51,12 +62,14 @@ const Individual = () => {
       </header>
       <div className="tarjeta">
         <div className="fichas">
-          <div className={`fichaTipo ${data.types[0].type.name}`}>Type</div>
-          <div className={`fichaTipo ${data.types[1].type.name}`}>Type</div>
+          {data?.types.map(
+            type => <div className={`fichaTipo ${type.type.name}`}> {type.type.name}</div>
+          )
+          }
         </div>
         <div className="tipo"></div>
         <div className="acerca">
-          <span className="sobre">About</span>
+          <span className={`sobre`}>About</span>
         </div>
         <div className="atributos">
           <div className="peso">
@@ -80,19 +93,24 @@ const Individual = () => {
           </div>
           <div className="divider"></div>
           <div className="movimientos">
-            <div className="tipoMovimientos"></div>
-            <span className="tipoMovimientos1">
-              {data?.abilities[0].ability.name}
-            </span>
-            <span className="tipoMovimientos2">
-              {data?.abilities[1].ability.name}
-            </span>
+            <div className="tipoMovimientos">
+              {data?.abilities.map(
+                ability => 
+                <span className="tipoMovimientos">
+                {ability.ability.name} 
+                </span>
+              )}
+             
+            </div>
+            
+                  
+            
             <span className="totalMovimientos">Moves</span>
           </div>
         </div>
         <div className="describe">
           <p className="descripcion">
-            <Skeleton count={5} />
+            {data?data.descripcion : <Skeleton/>}
           </p>
         </div>
         <div className="baseEstadisticas">
@@ -107,7 +125,9 @@ const Individual = () => {
             <span className="nombreValores">SDEF</span>
             <span className="nombreValores">SPD</span>
           </div>
+
           <div className="divider"></div>
+
           <div className="numerosEstadisticas">
             <span className="numeroValores">{data?.stats[0].base_stat}</span>
             <span className="numeroValores">{data?.stats[1].base_stat}</span>
@@ -117,42 +137,49 @@ const Individual = () => {
             <span className="numeroValores">{data?.stats[5].base_stat}</span>
           </div>
           <div className="barras">
+
             <div className="barraValores">
               <div className="marcaValores">
-                <div className="contenidoValores"></div>
+                <div className="contenidoValores" style={{width:`${data?.stats[0].base_stat}%`}}></div>
+                <div className="fondoValores" ></div>
+              </div>
+            </div>
+
+            <div className="barraValores">
+              <div className="marcaValores">
+                <div className="contenidoValores" style={{width:`${data?.stats[1].base_stat}%`}}></div>
+                <div className="fondoValores" ></div>
+              </div>
+            </div>
+
+            <div className="barraValores">
+              <div className="marcaValores">
+                <div className="contenidoValores" style={{width:`${data?.stats[2].base_stat}%`}}></div>
                 <div className="fondoValores"></div>
               </div>
             </div>
+
             <div className="barraValores">
               <div className="marcaValores">
-                <div className="contenidoValores"></div>
+                <div className="contenidoValores" style={{width:`${data?.stats[3].base_stat}%`}}></div>
                 <div className="fondoValores"></div>
               </div>
             </div>
+
             <div className="barraValores">
               <div className="marcaValores">
-                <div className="contenidoValores"></div>
+                <div className="contenidoValores" style={{width:`${data?.stats[4].base_stat}%`}}></div>
                 <div className="fondoValores"></div>
               </div>
             </div>
+
             <div className="barraValores">
               <div className="marcaValores">
-                <div className="contenidoValores"></div>
+                <div className="contenidoValores" style={{width:`${data?.stats[5].base_stat}%`}}></div>
                 <div className="fondoValores"></div>
               </div>
             </div>
-            <div className="barraValores">
-              <div className="marcaValores">
-                <div className="contenidoValores"></div>
-                <div className="fondoValores"></div>
-              </div>
-            </div>
-            <div className="barraValores">
-              <div className="marcaValores">
-                <div className="contenidoValores"></div>
-                <div className="fondoValores"></div>
-              </div>
-            </div>
+
           </div>
         </div>
       </div>
